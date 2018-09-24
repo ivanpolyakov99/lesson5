1e7009415c079217ebc1a791029dda1f659cbc11
ivanpolyakov99 <43088152+ivanpolyakov99@users.noreply.github.com>
2018-09-24 19:31:29 +0300

    Update HW5_3.py

--- HW5_3.py
+++ HW5_3.py
@@ -1,11 +1,15 @@
 def decorator_decorator(str1, str2):
         def inner(func_to_dec):
-            print(str1)
-            func_to_dec()
-            print(str2)
+                def inner1(*args, **kwargs):
+                        print(str1)
+                        func_to_dec(*args, **kwargs)
+                        print(str2)
+                return inner1
         return inner
 
 
 @decorator_decorator('1-ая строка до функции', '2-ая строка после функции')
-def func_to_dec():
+def func():
     print('Какой-то рандомный текст')
+
+func()
